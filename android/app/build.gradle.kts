3dc958760a85675a2f4e6af8318fa86844d99116
Eduardo Cortés <eduardo.cortes.herrera@proton.me>
2026-09-20 11:35:53 -0600

    feat(application): set custom bundle identifier and display name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flip_7_score_tracker"
+        applicationId = "com.eduardocortesherrera.flip7scoretracker"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
